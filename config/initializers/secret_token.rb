22ee8a3ba1e67cac309b8509edfd1432c1a8f653
Rajan Punchouty <rajan@ezzie.in>
2013-01-11 03:45:58 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DayThree::Application.config.secret_token = '5096add7d5e4f6dfb51169b3a0999724fcb72372cec8e8fce74f35b74657305f9b1c42b3d81ffd1922e449f328726663b192800aba7718f57c81d36c27db8fef'
